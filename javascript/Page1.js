cd63855e2cdfd03ccf319cd90a166534c1c70aa0
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-03-23 18:14:16 +0200

    My commit message

---
+++ javascript/Page1.js
@@ -0,0 +1,29 @@
+
+ const uploadImage = async (event) => {
+        const file = event.target.files[0];
+        const base64 = await convertBase64(file);
+        return base64;
+      };const convertBase64 = (file) => {
+          return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+          });
+        };
+document.getElementById('formFile').addEventListener("change", async(e) => {
+            let imageBase64 = await uploadImage(e);
+        document.getElementById('formFile').setAttribute('data-image-base64' ,imageBase64);
+        document.getElementById('formFile').setAttribute('name',e.target.files[0].name)
+        });
+$(
+      function () { $("#datepicker").datepicker({format: 'dd-mm-yyyy'}); }
+    );$(
+      function () { $("#datepicker-2").datepicker({format: 'dd-mm-yyyy'}); }
+    );window.onload = () => {};
